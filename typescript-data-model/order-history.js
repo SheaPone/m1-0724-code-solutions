fe3ac158dbbfb25d2355677af6de6fd25331db56
Shea Pone <sheapone@gmail.com>
2024-07-12 05:08:29 +0000

    Completed typescript-data-model exercise.

---
+++ typescript-data-model/order-history.js
@@ -0,0 +1,42 @@
+'use strict';
+const orderHistory = [
+  {
+    orderDate: 'August 4, 2020',
+    total: '$34',
+    orderNumber: '114-3941689-8772232',
+    deliveryDate: 'August 8, 2020',
+    objectOrdered: 'Book',
+    title: 'Javascript for impatient programmers',
+    author: 'Dr. Axel Rauschmayer',
+  },
+  {
+    orderDate: 'July 19, 2020',
+    total: '$44.53',
+    orderNumber: '113-9984268-1280257',
+    deliveryDate: 'July 20, 2020',
+    objectOrdered: 'Book',
+    title: 'The Timeless Way of Building',
+    author: 'Christopher Alexander',
+  },
+  {
+    orderDate: 'July 4, 2020',
+    total: '$17.22',
+    orderNumber: '114-2875557-9059409',
+    deliveryDate: 'July 7, 2020',
+    objectOrdered: 'Controller Adapter',
+    title:
+      'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+  },
+  {
+    orderDate: 'July 3, 2020',
+    total: '$138.93',
+    orderNumber: '113-2883177-2648248',
+    deliveryDate: 'July 5, 2020',
+    objectOrdered: 'GameCube Controller',
+    title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    secondPartOfOrder: {
+      title: 'The Art of Sql',
+      author: 'Stephanie Faroult',
+    },
+  },
+];
