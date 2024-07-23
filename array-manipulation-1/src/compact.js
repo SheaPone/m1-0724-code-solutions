380c04924f70b3647cac361d8218eeb981b19822
Shea Pone <sheapone@gmail.com>
2024-07-23 04:58:07 +0000

    Completed array-manipulation-1 exercise.

---
+++ array-manipulation-1/src/compact.js
@@ -0,0 +1,11 @@
+'use strict';
+/* exported compact */
+function compact(array) {
+  const newArray = [];
+  for (let index = 0; index < array.length; index++) {
+    if (array[index]) {
+      newArray.push(array[index]);
+    }
+  }
+  return newArray;
+}
